cda45963f951c0ac55ac0eee1055c5e519d22308
Sung-nyoun Kim <tityrus@gmail.com>
2016-03-01 21:40:09 +0900

    hello world circle

--- main.js
+++ main.js
@@ -12,9 +12,18 @@ $(document).ready(function(){
   //   }
   // }
   var tool = new Tool();
-  
+
   tool.onMouseDown = function(event){
     var c = Shape.Circle(event.point.x, event.point.y, 20);
     c.fillColor = 'green';
   };
+
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = 'black';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = 20;
+  text.content = 'hello world';  
+
 });
